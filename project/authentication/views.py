7428b49e45b08307e11a84fe70b4ffa59d9bad4e
mohamedmm <mohamed.mamdouh1510@gmail.com>
2023-01-08 03:16:19 +0200

    Regsiter & Login Pages , and template folder

--- project/authentication/views.py
+++ project/authentication/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 # Create your views here.
 
+
+
 def registerPage(request):
-    form = UserChangeForm()
+    form = UserCreationForm()
+    context = {"form":form}
     
-    if request.method == 'POST':
-        form = UserChangeForm(request.POST)
+    if request.method == 'POST': # if request.method equal to post 
+                                # then let's through the data to the form
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
     
-    context = {"form":form}
-    return render(request, 'register.html',context)
+    return render(request, 'register.html', context)
 
 def loginPage(request):
     context = {}
-    return render(request, 'login.html',context)
+    return render(request, 'login.html')
