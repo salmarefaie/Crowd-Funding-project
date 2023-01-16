6b7f4f0faa097b81ca7972fcac04e55c33c1ca1d
Mohamed El-Zakazeky <mohamed.mamdouh1510@gmail.com>
2023-01-17 00:50:52 +0200

    Login & Register

--- project/authentication/models.py
+++ project/authentication/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    data_created = models.DateTimeField(auto_now_add=True, null=True)
--- project/authentication/views.py
+++ project/authentication/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import *
+from .forms import OrderForm , CreateUserForm
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
     context = {"form":form}
     
-    if request.method == 'POST': # if request.method equal to post 
-                                # then let's through the data to the form
-        form = UserCreationForm(request.POST)
+    if request.method == 'POST': # if request.method equal to post # then let's through the data to the form
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
     
-    return render(request, 'register.html', context)
+    return render(request, 'Auth/register.html', context)
 
 def loginPage(request):
     context = {}
-    return render(request, 'login.html')
+    return render(request, 'Auth/login.html')
 def index(request):
     context = {}
     return render(request, 'index.html')
