d68bac8794d6343492623e76ab2762f3dde361e9
loaywalid <loaywalid1411@gmail.com>
2023-01-23 00:37:50 +0200

    fund model

--- project/fund/admin.py
+++ project/fund/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import fund
+
 
 # Register your models here.
--- project/fund/models.py
+++ project/fund/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class fund(models.Model):
+    projectname = models.CharField(max_length=50 , verbose_name="project name")
+    username = models.CharField(max_length=50 , verbose_name="user name")
+    useremail = models.CharField(max_length=50 , verbose_name="user email")
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name= "fund information"
