d20169f09cf8db466cf3ff24d34a3e448418c9e3
Mohamed El-Zakazeky <mohamed.mamdouh1510@gmail.com>
2023-01-22 17:06:26 +0200

    login and register page restrication update

--- project/authentication/views.py
+++ project/authentication/views.py
@@ -11,34 +11,39 @@ from django.contrib.auth.decorators import login_required
 
 # @login_required(login_url='login')
 def registerPage(request):
-    form = CreateUserForm()
-    context = {"form":form}
-    
-    if request.method == 'POST': # if request.method equal to post # then let's through the data to the form
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request,"Account was created for " + user)
-            return redirect('authentication:login')
+    if request.user.is_authenticated:
+        return redirect('authentication:home')
+    else:
+        form = CreateUserForm()
+        context = {"form":form}
+        
+        if request.method == 'POST': # if request.method equal to post # then let's through the data to the form
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,"Account was created for " + user)
+                return redirect('authentication:login')
         
         
     return render(request, 'Auth/register.html', context)
 
 def loginPage(request):
     context = {}
-    
-    if request.method == 'POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        
-        user = authenticate(username=username, password=password )
-        if user is not None:
-            login(request,user)
-            return redirect('authentication:home')
-        else:
-            messages.info(request, 'Username or Password is incorrect')
-            return render(request, 'Auth/login.html')
+    if request.user.is_authenticated:
+        return redirect('authentication:home')
+    else:
+        if request.method == 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            
+            user = authenticate(username=username, password=password )
+            if user is not None:
+                login(request,user)
+                return redirect('authentication:home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+                return render(request, 'Auth/login.html')
 
     return render(request, 'Auth/login.html')
 @login_required(login_url='login')
--- project/fund/views.py
+++ project/fund/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.contrib.auth.decorators import login_required
-@login_required(login_url='authentication:login')
 
+@login_required(login_url='authentication:login')
 def fundpage(request):
     return render (request , 'fund/fundpage.html')
 
